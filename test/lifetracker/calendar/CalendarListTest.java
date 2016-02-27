ae15eb6a03d17bc213c212d09d634b1e1eba1229
Shen Yichen <2007.yichen@gmail.com>
2016-02-27 13:41:12 +0800

    Add test template for CalendarList
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

---
+++ test/lifetracker/calendar/CalendarListTest.java
@@ -0,0 +1,14 @@
+package lifetracker.calendar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class CalendarListTest {
+
+	@Test
+	public void testGetTaskList() {
+		fail("Not yet implemented");
+	}
+
+}
