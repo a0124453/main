d199094d39876eb164e21c073529b4f77fa2f2f5
Nithi Chindaraksawong <nithi_chin@yahoo.com>
2016-03-01 01:39:14 +0800

    a lot more test cases for Parser

--- src/lifetracker/parser/Parser.java
+++ src/lifetracker/parser/Parser.java
@@ -41,19 +41,42 @@ public class Parser {
                 feedback = "\"" + task.trim() + "\" is added! It is due on " + endDate + " at " + endTime + ".";
 
             } else if (userInput.contains(" from ")) {
-                Pattern pattern = Pattern.compile("^(.+) from(\\s?([a-zA-Z]+))?(\\s?(\\d+))? to(\\s?([a-zA-Z]+))?(\\s?(\\d+))?$");
+                Pattern pattern = Pattern
+                        .compile("^(.+) from(\\s?([a-zA-Z]+))?(\\s?(\\d+))?(\\s(to))?(\\s?([a-zA-Z]+))?(\\s?(\\d+))?$");
                 Matcher matcher = pattern.matcher(userInput);
                 matcher.find();
                 task = matcher.group(1);
                 startDate = matcher.group(3);
                 startTime = matcher.group(5);
-                endDate = matcher.group(7);
-                endTime = matcher.group(9);
+                String toParam = matcher.group(7);
+                endDate = matcher.group(9);
+                endTime = matcher.group(11);
+
+                if (endTime == null) {
+                    endTime = "2359";
+                }
                 
-                feedback = String.format("\"%s\" is added! It is scheduled from %s at %s to %s at %s.",
-                        task, startDate, startTime, endDate, endTime);
+                if (startTime == null) {
+                    startTime = "currenttime";
+                }
                 
+                if (startDate == null) {
+                    startDate = "today";
+                }
                 
+                if (toParam == null) {
+                    int time = Integer.parseInt(startTime);
+                    endTime = Integer.toString(time + 100);
+                }
+
+                if (endDate == null) {
+                    feedback = String.format("\"%s\" is added! It is scheduled from %s at %s to %s.", task,
+                            startDate, startTime, endTime);
+                } else {
+                    feedback = String.format("\"%s\" is added! It is scheduled from %s at %s to %s at %s.", task,
+                            startDate, startTime, endDate, endTime);
+                }
+
             } else {
                 task = userInput;
                 feedback = "\"" + task.trim() + "\" is added!";
