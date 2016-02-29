d7eb3e9a643689da73feec120bcecb87e1277a35
Nithi Chindaraksawong <nithi_chin@yahoo.com>
2016-02-29 21:51:47 +0800

    create Parser Class

---
+++ test/lifetracker/parser/AddParserTest.java
@@ -0,0 +1,14 @@
+package lifetracker.parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddParserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
