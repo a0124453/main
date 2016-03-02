fed07904fff615dc608895c8e7554a6f2dbe1711
Shen Yichen <2007.yichen@gmail.com>
2016-03-02 23:15:23 +0800

    Add save for tasks
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/lifetracker/storage/ThreadedFileStorage.java
+++ src/lifetracker/storage/ThreadedFileStorage.java
@@ -2,6 +2,7 @@ package lifetracker.storage;
 
 import lifetracker.calendar.CalendarList;
 import lifetracker.calendar.Event;
+import lifetracker.calendar.Task;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -15,8 +16,10 @@ public class ThreadedFileStorage implements Storage {
 
     public static final String FILE_IS_DIRECTORY_ERROR = "Filename provided is actually a directory!";
 
+    public static final String NULL_DATETIME_MARKER = "NA";
+    public static final String FIELD_SEPARATOR = " ";
+
     private File storageFile;
-    private List<Event> eventList;
 
     public ThreadedFileStorage() throws IOException {
         this(DEFAULT_FILENAME);
@@ -40,7 +43,8 @@ public class ThreadedFileStorage implements Storage {
     @Override
     public void store(CalendarList calendar) throws IOException {
         try (BufferedWriter fileWrite = new BufferedWriter(new FileWriter(storageFile))) {
-            writeEvents(fileWrite, eventList);
+            writeEvents(fileWrite, calendar.getEventList());
+            writeTasks(fileWrite, calendar.getTaskList());
         }
     }
 
@@ -55,10 +59,26 @@ public class ThreadedFileStorage implements Storage {
         for (Event event : eventList) {
             writer.newLine();
             writer.write(event.getStart().toString());
-            writer.write(" ");
+            writer.write(FIELD_SEPARATOR);
             writer.write(event.getEnd().toString());
-            writer.write(" ");
+            writer.write(FIELD_SEPARATOR);
             writer.write(event.getName());
+        }
+    }
+
+    private void writeTasks(BufferedWriter writer, List<Task> taskList) throws IOException {
+        writer.write(String.valueOf(taskList.size()));
+
+        for (Task task : taskList) {
+            writer.newLine();
+
+            if (task.getDeadline() == null) {
+                writer.write(NULL_DATETIME_MARKER);
+            } else {
+                writer.write(task.getDeadline().toString());
+            }
+            writer.write(FIELD_SEPARATOR);
+            writer.write(task.getName());
         }
     }
 }
