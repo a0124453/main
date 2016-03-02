bb4e10b7df8c334d14955854dca80214baeed18b
Shen Yichen <2007.yichen@gmail.com>
2016-03-03 00:04:00 +0800

    Remove Interface
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/lifetracker/UI/UI.java
+++ src/lifetracker/UI/UI.java
@@ -2,9 +2,33 @@ package lifetracker.UI;
 
 import lifetracker.logic.Logic;
 
-public interface UI {
+import java.util.Scanner;
+
+public class UI {
+
+	private static final String MESSAGE_WELCOME = "Welcome to the Life Tracker, "
+			+ "Spend less time planning "
+			+ "so you always know what's next.";
+	private static final String MESSAGE_INPUT = "Command: ";
 	
-	void welcomeMessage();
+	static Scanner scanner = new Scanner(System.in);
 	
-	void executeUntilExit(Logic l);
+	public UI(Logic l){
+		welcomeMessage();
+		executeUntilExit(l);
+	}
+	
+	public void welcomeMessage() {
+		System.out.printf(MESSAGE_WELCOME);
+	}
+	
+	public void executeUntilExit(Logic l) {
+		while(true) {
+			System.out.printf(MESSAGE_INPUT);
+			String input = scanner.nextLine();
+			for (String resultLine: l.executeCommand(input).getResultLines()) {
+				System.out.println(resultLine);
+			}
+		}
+	}
 }
