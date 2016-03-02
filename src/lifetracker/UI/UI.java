e78bf7d3d4bf14c6b301d3c31fb0508bd81033ab
Shen Yichen <2007.yichen@gmail.com>
2016-03-03 00:08:44 +0800

    Tabs formatting
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/lifetracker/UI/UI.java
+++ src/lifetracker/UI/UI.java
@@ -7,35 +7,33 @@ import java.util.Scanner;
 
 public class UI {
 
-	private static final String MESSAGE_WELCOME = "Welcome to the Life Tracker, "
-			+ "Spend less time planning "
-			+ "so you always know what's next.";
-	private static final String MESSAGE_INPUT = "Command: ";
-	
-	static Scanner scanner = new Scanner(System.in);
-	
-	public UI(Logic l){
-		welcomeMessage();
-		executeUntilExit(l);
-	}
-	
-	public void welcomeMessage() {
-		System.out.printf(MESSAGE_WELCOME);
-	}
-	
-	public void executeUntilExit(Logic l) {
-		while(true) {
-			System.out.printf(MESSAGE_INPUT);
-			String input = scanner.nextLine();
+    private static final String MESSAGE_WELCOME = "Welcome to the Life Tracker, Spend less time planning so you always know what's next.";
+    private static final String MESSAGE_INPUT = "Command: ";
 
-			ExecuteResult result = l.executeCommand(input);
+    static Scanner scanner = new Scanner(System.in);
 
-			System.out.println(result.getComment());
-			System.out.println();
+    public UI(Logic l) {
+        welcomeMessage();
+        executeUntilExit(l);
+    }
 
-			for (String resultLine: result.getResultLines()) {
-				System.out.println(resultLine);
-			}
-		}
-	}
+    public void welcomeMessage() {
+        System.out.printf(MESSAGE_WELCOME);
+    }
+
+    public void executeUntilExit(Logic l) {
+        while (true) {
+            System.out.printf(MESSAGE_INPUT);
+            String input = scanner.nextLine();
+
+            ExecuteResult result = l.executeCommand(input);
+
+            System.out.println(result.getComment());
+            System.out.println();
+
+            for (String resultLine : result.getResultLines()) {
+                System.out.println(resultLine);
+            }
+        }
+    }
 }
