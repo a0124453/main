7a6e17ecf8b3bd6c2afe1e097b61605f102170bc
sshang0309 <sshang@connect.ust.hk>
2016-03-02 17:51:38 +0800

    welcome message

--- src/lifetracker/UI/NamedUI.java
+++ src/lifetracker/UI/NamedUI.java
@@ -1,7 +1,22 @@
 package lifetracker.UI;
 
+import java.util.Scanner;
+
 import lifetracker.logic.Logic;
 
 public class NamedUI implements UI {
 
+	private static final String MESSAGE_WELCOME = "Welcome to the Life Tracker, "
+			+ "Spend less time planning "
+			+ "so you always know what's next.";
+	
+	static Scanner scanner = new Scanner(System.in);
+	
+	public NamedUI(Logic l){
+		welcomeMessage();
+	}
+	
+	private static void welcomeMessage() {
+		System.out.printf(MESSAGE_WELCOME);
+	}
 }
