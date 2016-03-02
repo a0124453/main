d5a69104564a708f4c31bd65b25886022da97c88
Shen Yichen <2007.yichen@gmail.com>
2016-03-02 23:03:53 +0800

    Add Event save
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/lifetracker/storage/ThreadedFileStorage.java
+++ src/lifetracker/storage/ThreadedFileStorage.java
@@ -1,9 +1,13 @@
 package lifetracker.storage;
 
 import lifetracker.calendar.CalendarList;
+import lifetracker.calendar.Event;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class ThreadedFileStorage implements Storage {
 
@@ -12,13 +16,14 @@ public class ThreadedFileStorage implements Storage {
     public static final String FILE_IS_DIRECTORY_ERROR = "Filename provided is actually a directory!";
 
     private File storageFile;
+    private List<Event> eventList;
 
     public ThreadedFileStorage() throws IOException {
         this(DEFAULT_FILENAME);
     }
 
     public ThreadedFileStorage(String fileName) throws IOException {
-       setStore(fileName);
+        setStore(fileName);
     }
 
     @Override
@@ -33,12 +38,27 @@ public class ThreadedFileStorage implements Storage {
     }
 
     @Override
-    public void store(CalendarList calendar) {
-
+    public void store(CalendarList calendar) throws IOException {
+        try (BufferedWriter fileWrite = new BufferedWriter(new FileWriter(storageFile))) {
+            writeEvents(fileWrite, eventList);
+        }
     }
 
     @Override
     public CalendarList load() {
         return null;
+    }
+
+    private void writeEvents(BufferedWriter writer, List<Event> eventList) throws IOException {
+        writer.write(String.valueOf(eventList.size()));
+
+        for (Event event : eventList) {
+            writer.newLine();
+            writer.write(event.getStart().toString());
+            writer.write(" ");
+            writer.write(event.getEnd().toString());
+            writer.write(" ");
+            writer.write(event.getName());
+        }
     }
 }
