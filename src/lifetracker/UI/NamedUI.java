3c8b73e651af4de9c785ef3c5dba360525f83bf9
sshang0309 <sshang@connect.ust.hk>
2016-03-02 18:03:09 +0800

    execution until exit

--- src/lifetracker/UI/NamedUI.java
+++ src/lifetracker/UI/NamedUI.java
@@ -9,14 +9,26 @@ public class NamedUI implements UI {
 	private static final String MESSAGE_WELCOME = "Welcome to the Life Tracker, "
 			+ "Spend less time planning "
 			+ "so you always know what's next.";
+	private static final String MESSAGE_INPUT = "Command: ";
 	
 	static Scanner scanner = new Scanner(System.in);
 	
 	public NamedUI(Logic l){
 		welcomeMessage();
+		executeUntilExit(l);
 	}
 	
 	private static void welcomeMessage() {
 		System.out.printf(MESSAGE_WELCOME);
+	}
+	
+	private static void executeUntilExit(Logic l) {
+		while(true) {
+			System.out.printf(MESSAGE_INPUT);
+			String input = scanner.nextLine();
+			for (String resultLine: l.executeCommand(input).getResultLines()) {
+				System.out.println(resultLine);
+			}
+		}
 	}
 }
