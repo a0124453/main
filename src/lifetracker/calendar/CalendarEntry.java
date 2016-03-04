d92e2169d76a21dc064cf72a382c34c237b661b8
sshang0309 <sshang@connect.ust.hk>
2016-03-04 18:34:55 +0800

    create CalendarEntry

---
+++ src/lifetracker/calendar/CalendarEntry.java
@@ -0,0 +1,36 @@
+package lifetracker.calendar;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public interface CalendarEntry {
+
+    // get() and set() functions for variables
+    String getName();
+
+    void setName(String name);
+
+    LocalDateTime getStart();
+
+    void setStart(LocalDateTime start);
+
+    LocalDateTime getEnd();
+
+    void setEnd(LocalDateTime end);
+
+    LocalTime getStartTime();
+
+    LocalTime getEndTime();
+
+    boolean isToday();
+
+    boolean isOngoing();
+
+    boolean isOver();
+    
+    boolean isFloating();
+    
+    boolean isEvent();
+    
+    boolean isDeadline();
+}
