ee90583fb8a7a68321fdee22daf7270d012cc8d3
mhaikalr <mhaikalr1992@gmail.com>
2016-03-04 19:18:08 +0800

    Refactored CalendarList.java and CalendarListImpl.java. Merging of tasks and events into one interface.

--- src/lifetracker/calendar/CalendarListImpl.java
+++ src/lifetracker/calendar/CalendarListImpl.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class CalendarListImpl implements CalendarList {
 
     // variables
-    private List<Task> taskList = new ArrayList<>();
-    private List<Event> eventList = new ArrayList<>();
+    private List<CalendarEntryImpl> taskList = new ArrayList<>();
+    private List<CalendarEntryImpl> eventList = new ArrayList<>();
 
     // get() and set() functions for variables
 
@@ -18,7 +18,7 @@ public class CalendarListImpl implements CalendarList {
      * @see lifetracker.calendar.CalenderList#getTaskList()
      */
     @Override
-    public List<Task> getTaskList() {
+    public List<CalendarEntryImpl> getTaskList() {
         return taskList;
     }
 
@@ -28,52 +28,44 @@ public class CalendarListImpl implements CalendarList {
      * @see lifetracker.calendar.CalenderList#getEventList()
      */
     @Override
-    public List<Event> getEventList() {
+    public List<CalendarEntryImpl> getEventList() {
         return eventList;
     }
 
     /*
      * (non-Javadoc)
      *
-     * @see
-     * lifetracker.calendar.CalenderList#addEvent(lifetracker.calendar.Event)
-     */
-    @Override
-    public void addEvent(Event event) {
-        eventList.add(event);
-    }
-
-    /*
-     * (non-Javadoc)
-     *
-     * @see lifetracker.calendar.CalenderList#addEvent(java.lang.String,
-     * java.time.LocalDateTime, java.time.LocalDateTime)
-     */
-    @Override
-    public void addEvent(String name, LocalDateTime start, LocalDateTime end) {
-        Event e = new EventImpl(name, start, end);
-        eventList.add(e);
-    }
-
-    /*
-     * (non-Javadoc)
-     *
-     * @see lifetracker.calendar.CalenderList#addTask(lifetracker.calendar.Task)
-     */
-    @Override
-    public void addTask(Task task) {
-        taskList.add(task);
-    }
-
-    /*
-     * (non-Javadoc)
-     *
-     * @see lifetracker.calendar.CalenderList#addTask(java.lang.String,
+     * @see lifetracker.calendar.CalenderList#add(java.lang.String,
      * java.time.LocalDateTime)
      */
     @Override
-    public void addTask(String name, LocalDateTime deadline) {
-        Task t = new TaskImpl(name, deadline);
-        taskList.add(t);
+    public void add(String name) {
+        CalendarEntryImpl ft = new CalendarEntryImpl(name, null, null);
+        taskList.add(ft);
     }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see lifetracker.calendar.CalenderList#add(java.lang.String,
+     * java.time.LocalDateTime)
+     */
+    @Override
+    public void add(String name, LocalDateTime deadline) {
+        CalendarEntryImpl dt = new CalendarEntryImpl(name, null, deadline);
+        taskList.add(dt);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see lifetracker.calendar.CalenderList#add(java.lang.String,
+     * java.time.LocalDateTime, java.time.LocalDateTime)
+     */
+    @Override
+    public void add(String name, LocalDateTime start, LocalDateTime end) {
+        CalendarEntryImpl e = new CalendarEntryImpl(name, start, end);
+        eventList.add(e);
+    }
+
 }
