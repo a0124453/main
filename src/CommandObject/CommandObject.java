df8b16fd514206d1038bfb0240323a1d88191d08
Nithi Chindaraksawong <nithi_chin@yahoo.com>
2016-03-04 18:19:27 +0800

    create CommandObject Interface

--- src/CommandObject/CommandObject.java
+++ src/CommandObject/CommandObject.java
@@ -1,5 +1,13 @@
 package CommandObject;
+import lifetracker.calendar.CalendarList;
+
 
 public interface CommandObject {
+    
+    CalendarList execute(CalendarList calendar);
+    
+    CalendarList undo(CalendarList calendar);
+    
+    String getComments();
 
 }
