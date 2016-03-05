097e5432dc7e252f79b9ac4f31c56f8136fd2bdc
Shen Yichen <2007.yichen@gmail.com>
2016-03-05 14:05:01 +0800

    Add store test
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- test/lifetracker/storage/ThreadedFileStorageTest.java
+++ test/lifetracker/storage/ThreadedFileStorageTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ThreadedFileStorageTest {
@@ -60,7 +61,35 @@ public class ThreadedFileStorageTest {
 
     @Test
     public void testStore() throws Exception {
+        CalendarList emptyCalendar = new StorageCalendarStub(false);
 
+        storage.store(emptyCalendar);
+        storage.flush();
+
+        List<String> expectedFileContent = new ArrayList<>();
+        expectedEmptyFileContent.add("0");
+        expectedEmptyFileContent.add("0");
+
+        List<String> actualFileContent = Files.readAllLines(Paths.get(TEST_FILE_NAME));
+
+        Assert.assertEquals(expectedFileContent, actualFileContent);
+
+        CalendarList populatedCalendar = new StorageCalendarStub(true);
+
+        storage.store(populatedCalendar);
+        storage.flush();
+
+        expectedFileContent.clear();
+        expectedFileContent.add("2");
+        expectedFileContent.add("Test Task 1");
+        expectedFileContent.add("Test Task 2 2007-12-03T10:15:30 2016-03-14T23:59:59");
+        expectedFileContent.add("2");
+        expectedFileContent.add("Test Event 1 2016-03-14T23:59:59 2016-03-15T23:59:59");
+        expectedFileContent.add("Test Event 2 2016-03-14T11:59:59 2016-03-14T23:59:59");
+
+        actualFileContent = Files.readAllLines(Paths.get(TEST_FILE_NAME));
+
+        Assert.assertEquals(expectedFileContent, actualFileContent);
     }
 
     @Test
