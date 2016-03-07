56db19fc109e633c0b517d928ba4c9d23779b3a8
Nithi Chindaraksawong <nithi_chin@yahoo.com>
2016-03-07 18:32:09 +0800

    add some test case for DateParserTest

--- test/lifetracker/parser/DateParserTest.java
+++ test/lifetracker/parser/DateParserTest.java
@@ -2,13 +2,29 @@ package lifetracker.parser;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+
 import org.junit.Test;
 
 public class DateParserTest {
 
     @Test
     public void test() {
-        fail("Not yet implemented");
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("07/03/16"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("7/03/16"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("07/3/16"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("07/03/2016"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("07-03-16"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("7 mar 16"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("7 march 16"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("7 march 2016"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("7 mar"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("today"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("Today"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("Monday"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("mon"));
+        assertEquals(LocalDate.of(2016, 3, 7), DateParser.parseDate("m"));
+        
     }
 
 }
