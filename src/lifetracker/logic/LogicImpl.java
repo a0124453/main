fd83485ffe29e54f1f9b749524619c4318e5a942
Shen Yichen <2007.yichen@gmail.com>
2016-03-08 12:31:18 +0800

    Correct return type for Logic execute
    Logic will now make use of the command object to get results.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/lifetracker/logic/LogicImpl.java
+++ src/lifetracker/logic/LogicImpl.java
@@ -1,13 +1,23 @@
 package lifetracker.logic;
 
+import lifetracker.calendar.CalendarEntry;
 import lifetracker.calendar.CalendarList;
 import lifetracker.calendar.CalendarListImpl;
+import lifetracker.command.CommandObject;
 import lifetracker.parser.Parser;
 import lifetracker.storage.Storage;
 
 import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public class LogicImpl implements Logic {
+
+    private static final String ENTRY_COMMON_FORMAT = "%1$d: %2$s ";
+    private static final String DUAL_DATE_FORMAT = "%1$s - %2$s";
+
+    private static final FormatStyle DATE_STYLE = FormatStyle.MEDIUM;
+    private static final FormatStyle TIME_STYLE = FormatStyle.SHORT;
 
     private Parser commandParser;
     private Storage calendarStorage;
@@ -22,18 +32,32 @@ public class LogicImpl implements Logic {
 
     @Override
     public ExecuteResult executeCommand(String commandString) {
-        String parsedComment = commandParser.parse(commandString);
+        CommandObject commandToExecute = commandParser.parse(commandString);
+
+        CalendarList executedState = commandToExecute.execute(calendar);
 
         try {
-            calendarStorage.store(calendar);
             calendarStorage.store(calendar);
         } catch (IOException ex) {
             //TODO add exception handling
         }
 
         ExecuteResult runResult = new CommandLineResult();
-        runResult.setComment(parsedComment);
+        runResult.setComment(commandToExecute.getComment());
+
+        executedState.getTaskList().forEach(task -> runResult.addResultLine(taskSummary(task)));
 
         return runResult;
+    }
+
+    private static String taskSummary(CalendarEntry task) {
+
+        String returnString = String.format(ENTRY_COMMON_FORMAT, task.getId(), task.getName());
+
+        if (task.getEndTime() != null) {
+            returnString += task.getEndTime().format(DateTimeFormatter.ofLocalizedDateTime(DATE_STYLE, TIME_STYLE));
+        }
+
+        return returnString;
     }
 }
