4bf0bab64326814242d4e244a7fd95d8474e46ba
Shen Yichen <2007.yichen@gmail.com>
2016-03-08 22:40:12 +0800

    Add parse command body
    Added a method that employs an inverse stack to parse the command body.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/lifetracker/parser/CommandParser.java
+++ src/lifetracker/parser/CommandParser.java
@@ -2,9 +2,11 @@ package lifetracker.parser;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 import java.util.function.Predicate;
 
 /**
@@ -15,24 +17,25 @@ import java.util.function.Predicate;
  */
 public class CommandParser {
 
-    private final Set<String> commands;
+    private static final String COMMAND_BODY_SEPARATOR = " ";
 
+    private final Set<String> commands;
     /**
      * A map of keywords, together with their respective format verification predicates.
      */
     private final Map<String, Predicate<String>> keywordsWithVerifications;
     private final String defaultCommand;
-    private final String bodySeperator;
+    private final String fullCommandSeparator;
 
     public CommandParser(Set<String> commands,
             Map<String, Predicate<String>> keywordsWithVerifications,
             String defaultCommand,
-            String bodySeperator) {
+            String fullCommandSeparator) {
 
         this.commands = commands;
         this.keywordsWithVerifications = keywordsWithVerifications;
         this.defaultCommand = defaultCommand;
-        this.bodySeperator = bodySeperator;
+        this.fullCommandSeparator = fullCommandSeparator;
     }
 
     public List<String> parseFullCommand(String fullCommand) {
@@ -46,14 +49,56 @@ public class CommandParser {
 
         parsedComponents.add(command);
 
-        parsedComponents.addAll(Arrays.asList(body.split(bodySeperator)));
+        parsedComponents.addAll(Arrays.asList(body.split(fullCommandSeparator)));
 
         return parsedComponents;
 
     }
 
-    public Map<String, String> parseCommandBody(String commandBody, List<String> keywords) {
-        return null;
+    /**
+     * Parses the command body using the keywords supplied to this object.
+     * <p>
+     * This method parses the command from the back.
+     * <p>
+     * Arguments after a keyword is checked with the keyword predicate specified for validity. An invalid argument,
+     * repeated keyword, or end list will trigger the method to dump the arguments, together with the rest of the
+     * command into a entry accessible with key "name".
+     * <p>
+     * The method returns a map that can be iterated in parse order (i.e. components iterated from the back of the
+     * command). The keys are the keywords and the values are the arguments.
+     *
+     * @param commandBody The command body to parse
+     * @return A map with the components parsed
+     */
+    public Map<String, String> parseCommandBody(String commandBody) {
+
+        Map<String, String> keyWordArgumentMap = new LinkedHashMap<>();
+
+        Stack<String> processStack = commandBodyToReverseStack(commandBody);
+        Stack<String> intermediateStack = new Stack<>();
+
+        while (!processStack.isEmpty()){
+            String element = processStack.pop();
+
+            if(keywordsWithVerifications.containsKey(element)){
+                String argument = collapseList(intermediateStack);
+                intermediateStack.clear();
+
+                if(keywordsWithVerifications.get(element).test(argument)){
+                    keyWordArgumentMap.put(element, argument);
+                } else{
+                    processStack.push(element);
+                    processStack.push(argument);
+
+                    keyWordArgumentMap.put("name", collapseList(processStack));
+                    processStack.clear();
+                }
+            } else{
+                intermediateStack.push(element);
+            }
+        }
+
+        return keyWordArgumentMap;
     }
 
     private String augmentDefaultToFullCommand(String fullCommand) {
@@ -65,6 +110,34 @@ public class CommandParser {
             return defaultCommand + " " + fullCommand;
         }
 
+    }
+
+    /**
+     * Creates a stack from the command body, split into elements and pops from the back.
+     * @param commandBody The command body
+     * @return The reverse stack
+     */
+    private static Stack<String> commandBodyToReverseStack(String commandBody){
+        String[] splitCommand = commandBody.split(COMMAND_BODY_SEPARATOR);
+
+        Stack<String> processStack = new Stack<>();
+
+        for(String elem: splitCommand){
+            processStack.push(elem);
+        }
+
+        return processStack;
+    }
+
+    private static String collapseList(List<String> list){
+        StringBuilder collateString = new StringBuilder();
+
+        for(String elem: list){
+            collateString.append(elem);
+            collateString.append(" ");
+        }
+
+        return collateString.toString().trim();
     }
 
     private static String getFirstWord(String command) {
