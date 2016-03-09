715e5682cfd93a035718a15e05069b6ab2f1b0d8
Shen Yichen <2007.yichen@gmail.com>
2016-03-09 20:16:14 +0800

    Add DeleteCommand
    The DeleteCommand is built with an ID, and on execution, deletes the task with the ID from the calendar.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

---
+++ src/lifetracker/command/DeleteCommand.java
@@ -0,0 +1,35 @@
+package lifetracker.command;
+
+import lifetracker.calendar.CalendarList;
+
+public class DeleteCommand implements CommandObject {
+
+    private static final String MESSAGE_DELETED = "%1$d is deleted.";
+    private static final String MESSAGE_NOT_FOUND = "%1$d cannot be found!";
+
+    private final int entryID;
+    private String comment = MESSAGE_ERROR;
+
+    public DeleteCommand(int entryID) {
+        this.entryID = entryID;
+    }
+
+    @Override
+    public CalendarList execute(CalendarList calendar) {
+
+        calendar.delete(entryID);
+
+        comment = String.format(MESSAGE_DELETED, entryID);
+        return null;
+    }
+
+    @Override
+    public CalendarList undo(CalendarList calendar) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getComment() {
+        return this.comment;
+    }
+}
