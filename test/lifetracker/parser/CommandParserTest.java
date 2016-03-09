3ceace0f2752f04e4b6d937d62e8d62fe7dbfa76
Shen Yichen <2007.yichen@gmail.com>
2016-03-09 17:39:07 +0800

    Add test for command body parsing
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- test/lifetracker/parser/CommandParserTest.java
+++ test/lifetracker/parser/CommandParserTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -25,13 +26,16 @@ public class CommandParserTest {
     {
         keyWordVerification.put("three", s -> s.length() == 3);
         keyWordVerification.put("empty", String::isEmpty);
+        keyWordVerification.put("empty2", String::isEmpty);
+        keyWordVerification.put("anything", s -> true);
     }
 
     private String defaultCommand = "add";
 
     private String fullCommandSeperator = "| ";
 
-    private CommandParser cmdParser = new CommandParser(commands, keyWordVerification, defaultCommand, fullCommandSeperator);
+    private CommandParser cmdParser = new CommandParser(commands, keyWordVerification, defaultCommand,
+            fullCommandSeperator);
 
     @Test
     public void testParseFullCommand() throws Exception {
@@ -71,5 +75,39 @@ public class CommandParserTest {
     @Test
     public void testParseCommandBody() throws Exception {
 
+        String commandBody = "name empty three abc";
+
+        Map<String, String> expected = new LinkedHashMap<>();
+
+        expected.put("three", "abc");
+        expected.put("empty", "");
+        expected.put("name", "name");
+
+        Assert.assertEquals(expected, cmdParser.parseCommandBody(commandBody));
+
+        commandBody = "name anything abs anything abc def three abc";
+
+        expected.clear();
+        expected.put("three", "abc");
+        expected.put("anything", "abc def");
+        expected.put("name", "name anything abs");
+
+        Assert.assertEquals(expected, cmdParser.parseCommandBody(commandBody));
+
+        commandBody = "anything something something empty aaa";
+
+        expected.clear();
+        expected.put("name", "anything something something empty aaa");
+
+        Assert.assertEquals(expected, cmdParser.parseCommandBody(commandBody));
+
+        commandBody = "empty empty2";
+
+        expected.clear();
+        expected.put("empty2", "");
+        expected.put("empty", "");
+        expected.put("name", "");
+
+        Assert.assertEquals(expected, cmdParser.parseCommandBody(commandBody));
     }
 }
