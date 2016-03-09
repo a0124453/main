651d02426d355f62db9599416f455b8ba29f5253
Shen Yichen <2007.yichen@gmail.com>
2016-03-09 20:36:23 +0800

    Add EditCommand
    Edit Command is initialized with and ID, and all the parameters to change.
    
    On execute, it calls the CalendarList update method.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

---
+++ src/lifetracker/command/EditCommand.java
@@ -0,0 +1,44 @@
+package lifetracker.command;
+
+import lifetracker.calendar.CalendarList;
+
+import java.time.LocalDateTime;
+
+public class EditCommand implements CommandObject {
+
+    private static final String MESSAGE_EDITED = "\"%1$d\" is edited.";
+
+    private final int entryID;
+    private final String name;
+    private final LocalDateTime startTime;
+    private final LocalDateTime endTime;
+
+    private String comment = MESSAGE_ERROR;
+
+    public EditCommand(int entryID, String name, LocalDateTime startTime, LocalDateTime endTime) {
+        this.entryID = entryID;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public CalendarList execute(CalendarList calendar) {
+
+        calendar.update(entryID, name, startTime, endTime);
+
+        comment = String.format(MESSAGE_EDITED, entryID);
+
+        return null;
+    }
+
+    @Override
+    public CalendarList undo(CalendarList calendar) {
+        return null;
+    }
+
+    @Override
+    public String getComment() {
+        return comment;
+    }
+}
