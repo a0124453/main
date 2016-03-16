8ee721055056eebb759bfa2ad8be66782ada383f
Shen Yichen <2007.yichen@gmail.com>
2016-03-17 00:41:38 +0800

    Add logging for storage thread
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/lifetracker/storage/FileStoreProcess.java
+++ src/lifetracker/storage/FileStoreProcess.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * A class that writes lines to a file.
@@ -16,6 +18,15 @@ import java.util.concurrent.PriorityBlockingQueue;
  * This class is designed to be run by a single thread. Running it on multiple threads might cause concurrency issues.
  */
 public class FileStoreProcess implements Runnable {
+
+    private static final Logger STORE_LOG = Logger.getGlobal();
+
+    private static final String LOG_ENTRY_ADDED = "Storage Thread: Storage entry %1$d added.";
+    private static final String LOG_SHUTDOWN_ENTRY_ADDED = "Storage Thread: Shutdown entry added.";
+    private static final String LOG_FILE_WRITTEN = "Storage Thread: Entry %1$d written to file.";
+    private static final String LOG_ENTRY_SKIPPED = "Storage Thread: Storage entry %1$d skipped.";
+    private static final String LOG_STARTUP = "Storage Thread: Starting";
+    private static final String LOG_SHUTDOWN = "Storage Thread: Exiting";
 
     private static final String ERROR_INVALID_FILE = "File is null or is invalid!";
     private static final String ERROR_WAIT_INTERRUPTED = "FileStoreProcess was interrupted while waiting for data to write.";
@@ -45,7 +56,11 @@ public class FileStoreProcess implements Runnable {
     public void submitSaveList(List<String> saveList) {
         assert saveList != null;
 
-        writeQueue.add(new WriteNode(new ArrayList<>(saveList)));
+        WriteNode newNode = new WriteNode(new ArrayList<>(saveList));
+
+        writeQueue.add(newNode);
+
+        STORE_LOG.log(Level.INFO, String.format(LOG_ENTRY_ADDED, newNode.getSequenceNum()));
     }
 
     /**
@@ -55,10 +70,13 @@ public class FileStoreProcess implements Runnable {
      */
     public void submitClose() {
         writeQueue.add(WriteNode.END_NODE);
+        STORE_LOG.log(Level.INFO, LOG_SHUTDOWN_ENTRY_ADDED);
     }
 
     @Override
     public void run() {
+
+        STORE_LOG.log(Level.INFO, LOG_STARTUP);
 
         long latestWrittenNodeNum = Long.MIN_VALUE;
 
@@ -72,10 +90,17 @@ public class FileStoreProcess implements Runnable {
                     latestWrittenNodeNum = currentNode.getSequenceNum();
 
                     writeNodeToFile(currentNode);
+
+                    STORE_LOG.log(Level.INFO, String.format(LOG_FILE_WRITTEN, currentNode.getSequenceNum()));
+                } else {
+                    STORE_LOG.log(Level.WARNING, String.format(LOG_ENTRY_SKIPPED, currentNode.getSequenceNum()));
                 }
 
                 currentNode = writeQueue.take();
             }
+
+            STORE_LOG.log(Level.INFO, LOG_SHUTDOWN);
+
         } catch (InterruptedException e) {
             System.err.println(ERROR_WAIT_INTERRUPTED);
         } catch (IOException e) {
--- test/lifetracker/parser/AddParserTest.java
+++ test/lifetracker/parser/AddParserTest.java
@@ -1,69 +1,11 @@
 package lifetracker.parser;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
-
-import lifetracker.parser.Parser;
 
 public class AddParserTest {
 
     @Test
     public void addParserTest() {
-        ParserImpl parse;
-        parse = new ParserImpl("");
-        assertEquals("invalid command!", parse.feedback);
-        
-        parse = new ParserImpl("add task");
-        assertEquals("\"task\" is added!", parse.feedback);
-        
-        parse = new ParserImpl("task");
-        assertEquals("\"task\" is added!", parse.feedback);
 
-        parse = new ParserImpl("task by Sunday 1200");
-        assertEquals("\"task\" is added! It is due on Sunday at 1200.", parse.feedback);   
-        parse = new ParserImpl("task by 1200");
-        assertEquals("\"task\" is added! It is due on today at 1200.", parse.feedback);
-        
-        parse = new ParserImpl("add task by Sunday");
-        assertEquals("\"task\" is added! It is due on Sunday at 2359.", parse.feedback);
-        parse = new ParserImpl("add task by 1200");
-        assertEquals("\"task\" is added! It is due on today at 1200.", parse.feedback);
-        
-        parse  = new ParserImpl("event from monday 1200 to sunday 1300");
-        assertEquals("\"event\" is added! It is scheduled from monday at 1200 to sunday at 1300.", parse.feedback);
-        parse  = new ParserImpl("event from monday 1200 to sunday");
-        assertEquals("\"event\" is added! It is scheduled from monday at 1200 to sunday at 2359.", parse.feedback);      
-        parse  = new ParserImpl("event from monday 1200 to 1400");
-        assertEquals("\"event\" is added! It is scheduled from monday at 1200 to 1400.", parse.feedback);
-        parse  = new ParserImpl("event from monday 1200");
-        assertEquals("\"event\" is added! It is scheduled from monday at 1200 to 1300.", parse.feedback);
-        
-        // no start time
-        parse  = new ParserImpl("event from monday to sunday 1300");
-        assertEquals("\"event\" is added! It is scheduled from monday at currenttime to sunday at 1300.", parse.feedback);
-        parse  = new ParserImpl("event from monday to sunday");
-        assertEquals("\"event\" is added! It is scheduled from monday at currenttime to sunday at 2359.", parse.feedback);
-        parse  = new ParserImpl("event from monday to 1300");
-        assertEquals("\"event\" is added! It is scheduled from monday at currenttime to 1300.", parse.feedback);
-        
-        // this fail since current time is cant be convert to integer
-        /*
-        parse  = new Parser("event from monday");
-        assertEquals("\"event\" is added! It is scheduled from monday at currenttime to 2359.", parse.feedback);
-        */
-        
-        // no start date
-        parse  = new ParserImpl("event from 1200 to sunday 1300");
-        assertEquals("\"event\" is added! It is scheduled from today at 1200 to sunday at 1300.", parse.feedback);
-        parse  = new ParserImpl("event from 1200");
-        assertEquals("\"event\" is added! It is scheduled from today at 1200 to 1300.", parse.feedback);
-        parse  = new ParserImpl("event from 1200 to sunday");
-        assertEquals("\"event\" is added! It is scheduled from today at 1200 to sunday at 2359.", parse.feedback);
-        parse  = new ParserImpl("event from 1200 to 1400");
-        assertEquals("\"event\" is added! It is scheduled from today at 1200 to 1400.", parse.feedback);
-        
-        
     }
-
 }
