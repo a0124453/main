c06b0839023f25ea1cbe0f20727f372e8fa17f4c
sshang0309 <sshang@connect.ust.hk>
2016-03-23 00:12:20 +0800

    handle IOexception inside the before class

--- test/lifetracker/logic/LogicImplTest.java
+++ test/lifetracker/logic/LogicImplTest.java
@@ -2,6 +2,8 @@ package lifetracker.logic;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -12,14 +14,16 @@ public class LogicImplTest {
     
     private static Parser parser = new LogicParserStub();
     private static Storage storage = new LogicStorageStub();
-    //private static LogicImpl logicTest = new LogicImpl(parser, storage);
+    private static LogicImpl logicTest;
     
     private static ExecuteResult expected1 = new CommandLineResult();
     private static ExecuteResult expected2 = new CommandLineResult();
     private static ExecuteResult expected3 = new CommandLineResult();
     
     @BeforeClass
-    public static void setUpBeforeClass() {
+    public static void setUpBeforeClass() throws IOException {
+        logicTest = new LogicImpl(parser, storage);
+        
         expected1.setComment("\"first meeting\" is added.");
         expected1.setType("add first meeting");
         
@@ -30,10 +34,10 @@ public class LogicImplTest {
         expected3.setType("add third meeting from 2016-10-01 23:59:59 to 2016-11-01 23:59:59");
     }
 
-    /*@Test
+    @Test
     public void testAdd() {
         assertEquals(expected1, logicTest.executeCommand("add first meeting"));
         assertEquals(expected2, logicTest.executeCommand("add second meeting by 2016-09-01 23:59:59"));
         assertEquals(expected3, logicTest.executeCommand("add third meeting from 2016-10-01 23:59:59 to 2016-11-01 23:59:59"));
-    }*/
+    }
 }
