fae6e9b8bcf5148186544e0ff818028954cdae32
sshang0309 <sshang@connect.ust.hk>
2016-03-23 22:50:57 +0800

    modify the parser stub for logic to handle exception

--- test/lifetracker/logic/LogicParserStub.java
+++ test/lifetracker/logic/LogicParserStub.java
@@ -2,6 +2,7 @@ package lifetracker.logic;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import lifetracker.command.CommandObject;
 import lifetracker.parser.Parser;;
@@ -17,22 +18,30 @@ public class LogicParserStub implements Parser {
         if(userInput.contains("by")) {
             String[] content = item.split(" by ");
             String name = content[0];
-            LocalDateTime deadline = LocalDateTime.parse(content[1],
-                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            return new LogicCommandStub(name, deadline);
+            
+            try {
+                LocalDateTime deadline = LocalDateTime.parse(content[1],
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                return new LogicCommandStub(name, deadline);
+            } catch (DateTimeParseException e) {
+                throw new IllegalArgumentException();
+            }
         }
         
         else if(userInput.contains("from")) {
             String[] content = item.split(" from ");
             String name = content[0];
             
-            String[] startEnd = content[1].split(" to ");
-            LocalDateTime start = LocalDateTime.parse(startEnd[0],
-                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            LocalDateTime end = LocalDateTime.parse(startEnd[1],
-                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            
-            return new LogicCommandStub(name, start, end);
+            try {
+                String[] startEnd = content[1].split(" to ");
+                LocalDateTime start = LocalDateTime.parse(startEnd[0],
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                LocalDateTime end = LocalDateTime.parse(startEnd[1],
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                return new LogicCommandStub(name, start, end);
+            } catch (DateTimeParseException e) {
+                throw new IllegalArgumentException();
+            }
         }
         
         else {
