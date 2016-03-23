e7e2f7c9bc93d2511e3e9570273903baefa57fa6
mhaikalr <mhaikalr1992@gmail.com>
2016-03-23 18:03:29 +0800

    Updated tests.
    Ignore commit. Will be refined later.

---
+++ test/lifetracker/calendar/CalendarEntryTest.java
@@ -0,0 +1,44 @@
+package lifetracker.calendar;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalendarEntryTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testIsToday() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsOngoing() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsOver() {
+        fail("Not yet implemented");
+    }
+
+}
--- test/lifetracker/calendar/CalendarListTest.java
+++ test/lifetracker/calendar/CalendarListTest.java
@@ -1,55 +1,68 @@
 package lifetracker.calendar;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class CalendarListTest {
 
-    public static final int listSize = 10;
-    public static final LocalDateTime now = LocalDateTime.now();
+    public static final int NO_OF_FLOATING_TASKS = 3;
+    public static final int NO_OF_DEADLINE_TASKS = 3;
+    public static final int TOTAL_NO_OF_TASKS = 6;
+    public static final int TOTAL_NO_OF_EVENTS = 3;
+    public static final int TOTAL_NO_OF_ENTRIES = 9;
+    public static final LocalDateTime NOW = LocalDateTime.now();
+    public static final LocalDateTime TWO_HOURS_LATER = LocalDateTime.now().plusHours(2);
 
-    private static CalendarListImpl testCalendar = new CalendarListImpl();
+    private static CalendarList testCalendar = new CalendarListImpl();
     private static List<CalendarEntry> expectedTaskList = new ArrayList<>();
+    private static List<CalendarEntry> expectedEventList = new ArrayList<>();
 
     @BeforeClass
     public static void setUpBeforeClass() {
-        // add tasks to testCalendar using overloaded addTask methods
-        for (int i = 0; i < listSize; i++) {
-            if (i < listSize / 2) {
-                // addTask(String)
+        // add tasks to testCalendar using overloaded add methods
+        for (int i = 0; i < TOTAL_NO_OF_ENTRIES; i++) {
+            if (i < NO_OF_FLOATING_TASKS) {
+                // add(String)
                 testCalendar.add(Integer.toString(i));
+            } else if (i < TOTAL_NO_OF_TASKS) {
+                // add(String, LocalDateTime)
+                testCalendar.add(Integer.toString(i), NOW);
             } else {
-                // addTask(String, LocalDateTime)
-                testCalendar.add(Integer.toString(i), now);
+                // add(String, LocalDateTime, LocalDateTime)
+                testCalendar.add(Integer.toString(i), NOW, TWO_HOURS_LATER);
             }
         }
-        // set up testTaskList for comparison with task list in testCalendar
-        for (int i = 0; i < listSize; i++) {
-            if (i < listSize / 2) {
-                CalendarEntryImpl task = new CalendarEntryImpl(Integer.toString(i), null, null, i);
-                expectedTaskList.add(task);
+        // set up expectedTaskList and expectedEventList for comparison with
+        // lists in testCalendar
+        for (int i = 0; i < TOTAL_NO_OF_ENTRIES; i++) {
+            if (i < NO_OF_FLOATING_TASKS) {
+                CalendarEntry entry = new CalendarEntryImpl(Integer.toString(i), null, null, i);
+                expectedTaskList.add(entry);
+            } else if (i < TOTAL_NO_OF_TASKS) {
+                CalendarEntry entry = new CalendarEntryImpl(Integer.toString(i), null, NOW, i);
+                expectedTaskList.add(entry);
             } else {
-                CalendarEntryImpl task = new CalendarEntryImpl(Integer.toString(i),null ,now, i);
-                expectedTaskList.add(task);
+                CalendarEntry entry = new CalendarEntryImpl(Integer.toString(i), NOW, TWO_HOURS_LATER, i);
+                expectedEventList.add(entry);
             }
         }
 
     }
 
     @Test
-    public void runTest() {
+    public void testAdd() {
         // compare names of all tasks in both lists
-        for (int i = 0; i < listSize; i++) {
+        for (int i = 0; i < TOTAL_NO_OF_TASKS; i++) {
             assertEquals(expectedTaskList.get(i).getName(), testCalendar.getTaskList().get(i).getName());
         }
         // compare deadlines of all tasks in both lists
-        for (int i = 0; i < listSize; i++) {
+        for (int i = 0; i < TOTAL_NO_OF_TASKS; i++) {
             assertEquals(expectedTaskList.get(i).getEnd(), testCalendar.getTaskList().get(i).getEnd());
         }
     }
