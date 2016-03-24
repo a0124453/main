17c0a6f8d02ea9d07736a8f3ec7194ba673958f2
Nithi Chindaraksawong <nithi_chin@yahoo.com>
2016-03-24 14:56:13 +0800

    addd few more test case to delete

--- test/lifetracker/command/DeleteCommandTest.java
+++ test/lifetracker/command/DeleteCommandTest.java
@@ -1,9 +1,16 @@
 package lifetracker.command;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.time.LocalDateTime;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import lifetracker.calendar.CalendarList;
+import lifetracker.calendar.CalendarListImpl;
 
 public class DeleteCommandTest {
     
@@ -12,4 +19,29 @@ public class DeleteCommandTest {
         DeleteCommand deleteCommand = new DeleteCommand(1);
         Assert.assertEquals(deleteCommand.getComment(), CommandObject.MESSAGE_ERROR);
     }
+    
+    @Test
+    public void testExecute() throws Exception {
+
+        CalendarListImpl mockedCalendar = new CalendarListImpl();
+        
+        mockedCalendar.add("task");
+        
+        DeleteCommand deleteCommand = new DeleteCommand(1);
+        deleteCommand.execute(mockedCalendar);
+        
+        Assert.assertEquals("1 is deleted.", deleteCommand.getComment());
+        
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testFailExecute() throws IllegalArgumentException {
+
+        CalendarListImpl mockedCalendar = new CalendarListImpl();       
+        DeleteCommand deleteCommand = new DeleteCommand(1);
+        deleteCommand.execute(mockedCalendar);
+
+    }
+    
+    
 }
