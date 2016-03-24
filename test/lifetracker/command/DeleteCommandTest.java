ab9833e679fd6f364febcf624cdd25a2c1b8d927
Nithi Chindaraksawong <nithi_chin@yahoo.com>
2016-03-24 14:34:49 +0800

    add TestUnexecutedComment

--- test/lifetracker/command/DeleteCommandTest.java
+++ test/lifetracker/command/DeleteCommandTest.java
@@ -2,13 +2,14 @@ package lifetracker.command;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DeleteCommandTest {
-
+    
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testUnexecutedComment() throws Exception {
+        DeleteCommand deleteCommand = new DeleteCommand(1);
+        Assert.assertEquals(deleteCommand.getComment(), CommandObject.MESSAGE_ERROR);
     }
-
 }
