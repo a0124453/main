ff968a0b1b8756db3f514bee72a603aea5a46759
Nithi Chindaraksawong <nithi_chin@yahoo.com>
2016-03-24 14:31:19 +0800

    create DeleteCommandTest

---
+++ test/lifetracker/command/DeleteCommandTest.java
@@ -0,0 +1,14 @@
+package lifetracker.command;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeleteCommandTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
