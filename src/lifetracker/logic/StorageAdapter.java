359c2fc7e5c9bca5755a442264a9063bb10877e2
sshang0309 <sshang@connect.ust.hk>
2016-03-28 04:50:43 +0800

    implements storage adapter

--- src/lifetracker/logic/StorageAdapter.java
+++ src/lifetracker/logic/StorageAdapter.java
@@ -1,10 +1,13 @@
 package lifetracker.logic;
 
+import java.io.IOException;
+
 import lifetracker.calendar.CalendarList;
+import lifetracker.storage.Storage;
 
 public interface StorageAdapter {
     
-    void store(CalendarList calendar);
+    void store(CalendarList calendar, Storage storage) throws IOException;
     
-    CalendarList load(CalendarList calendar);
+    CalendarList load(Storage storage) throws IOException;
 }
--- src/lifetracker/logic/StorageAdapterImpl.java
+++ src/lifetracker/logic/StorageAdapterImpl.java
@@ -1,5 +1,27 @@
 package lifetracker.logic;
 
-public class StorageAdapterImpl {
+import java.io.IOException;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import lifetracker.calendar.CalendarList;
+import lifetracker.storage.Storage;
+
+public class StorageAdapterImpl implements StorageAdapter {
+
+    @Override
+    public void store(CalendarList calendar, Storage storage) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String s = gson.toJson(calendar);
+        storage.store(s);
+    }
+    
+    @Override
+    public CalendarList load(Storage storage) throws IOException {
+        String l = storage.load();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        CalendarList calendar = gson.fromJson(l, CalendarList.class);
+        return calendar;
+    }
 }
