aad831e19c80e8f170e183520c972f9982007e1e
Nithi Chindaraksawong <nithi_chin@yahoo.com>
2016-03-28 22:39:18 +0800

    implement UILogic to take in userInput

--- src/lifetracker/UI/UILogic.java
+++ src/lifetracker/UI/UILogic.java
@@ -1,5 +1,18 @@
 package lifetracker.UI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class UILogic {
 
+    String input;
+    
+    @FXML
+    TextField textInput;
+    
+    @FXML
+    public void getInput() {
+        input = textInput.getText();
+        textInput.setText("");
+    }
 }
