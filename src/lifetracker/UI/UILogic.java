d81cf661fbe751d94eb14b492fe5d095e1fe79ef
Nithi Chindaraksawong <nithi_chin@yahoo.com>
2016-03-28 22:54:33 +0800

    add support for exit command

--- src/lifetracker/UI/UILogic.java
+++ src/lifetracker/UI/UILogic.java
@@ -6,15 +6,19 @@ import javafx.scene.control.TextField;
 public class UILogic {
 
     String userInput;
-    
+
     @FXML
     TextField textInput;
-    
+
     @FXML
     public void getInput() {
-        userInput = textInput.getText();
-        System.out.println(userInput);
-        textInput.setText("");
-        textInput.setPromptText("Enter Command");
+
+        if (!textInput.getText().toLowerCase().equals("exit")) {
+            userInput = textInput.getText();
+            System.out.println(userInput);
+            textInput.setText("");
+        } else {
+            System.exit(0);
+        }
     }
 }
