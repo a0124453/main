348f0ac31cd44c39245c1307e80b63073651863d
Nithi Chindaraksawong <nithi_chin@yahoo.com>
2016-03-28 23:22:13 +0800

    change UILogic to UIController

--- src/lifetracker/LifeTracker.java
+++ src/lifetracker/LifeTracker.java
@@ -14,6 +14,8 @@ import java.util.logging.FileHandler;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
+import com.sun.j3d.utils.scenegraph.io.retained.Controller;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -60,6 +62,7 @@ public class LifeTracker extends Application {
         scene.getStylesheets().add(getClass().getResource("/lifetracker/UI/application.css").toExternalForm());
         primaryStage.setTitle("Life Tracker");
         primaryStage.setScene(scene);
+        
         primaryStage.show();
     }
 }
--- src/lifetracker/UI/UIController.java
+++ src/lifetracker/UI/UIController.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class UILogic {
+public class UIController {
 
     String userInput;
 
