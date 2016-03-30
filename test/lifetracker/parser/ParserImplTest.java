4f713c1384408659167edfcc7482b8348ad9c3be
Shen Yichen <2007.yichen@gmail.com>
2016-03-30 23:33:11 +0800

    Add tests for recurring deadlines
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- test/lifetracker/parser/ParserImplTest.java
+++ test/lifetracker/parser/ParserImplTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.TemporalAmount;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -46,10 +48,6 @@ public class ParserImplTest {
         verify(cmdFactory).addDeadlineTask("do homework", expectedDateTime);
         //Various datetime formats are tested in DateTimeParserTest
 
-        //Boundary: No command
-        parser.parse("email client by tomorrow 2pm");
-        verify(cmdFactory).addDeadlineTask("email client", expectedDateTime);
-
         //Boundary: Invalid end date
         parser.parse("drop by supermarket");
         verify(cmdFactory).addFloatingTask("drop by supermarket");
@@ -57,5 +55,32 @@ public class ParserImplTest {
         //Boundary: 2 date times
         parser.parse("drop by 7/11 by tomorrow 2pm");
         verify(cmdFactory).addDeadlineTask("drop by 7/11", expectedDateTime);
+    }
+
+    @Test
+    public void testParseAddRecurringDeadline() throws Exception {
+        //Partition: Recurring deadlines
+        parser.parse("finish readings by tomorrow 2pm every 2 week");
+        LocalDateTime expectedDateTime = LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(14, 0));
+        TemporalAmount expectedRecurringDuration = Period.ofWeeks(2);
+        verify(cmdFactory).addRecurringDeadlineTask("finish readings", expectedDateTime, expectedRecurringDuration);
+
+        //Boundary: Missing date time
+        parser.parse("water plants every day");
+        verify(cmdFactory).addRecurringDeadlineTask("water plants",
+                LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT.minusMinutes(1)), Period.ofDays(1));
+
+        //Boundary: Inverted options
+        parser.parse("check out stall every 2 weeks by tomorrow 2pm");
+        verify(cmdFactory).addRecurringDeadlineTask("check out stall", expectedDateTime, expectedRecurringDuration);
+
+        //Boundary: Invalid durations
+        parser.parse("check every single thing");
+        verify(cmdFactory).addFloatingTask("check every single thing");
+    }
+
+    @Test
+    public void testParseAddEvent() throws Exception {
+
     }
 }
