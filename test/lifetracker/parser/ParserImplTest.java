10d7fc350f452c773f24248b95cd62c07a11e3f9
Shen Yichen <2007.yichen@gmail.com>
2016-03-30 23:54:21 +0800

    Add rest of add test cases
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- test/lifetracker/parser/ParserImplTest.java
+++ test/lifetracker/parser/ParserImplTest.java
@@ -81,6 +81,35 @@ public class ParserImplTest {
 
     @Test
     public void testParseAddEvent() throws Exception {
+        //Partition: Add events
+        parser.parse("CS2103 Exam from 25/4/16 1pm to 3pm");
+        verify(cmdFactory)
+                .addEvent("CS2103 Exam", LocalDateTime.of(2016, 4, 25, 13, 0), LocalDateTime.of(2016, 4, 25, 15, 0));
 
+        //Boundary: Inverted options
+        parser.parse("Exam to 3pm from 25/4/16 1pm");
+        verify(cmdFactory).addEvent("Exam", LocalDateTime.of(2016, 4, 25, 13, 0), LocalDateTime.of(2016, 4, 25, 15, 0));
+
+        //Boundary: Invalid date time
+        parser.parse("meeting from 2pm to hello");
+        verify(cmdFactory).addFloatingTask("meeting from 2pm to hello");
+
+        //Boundary: Missing to
+        parser.parse("some talk from 22/4/16 10am");
+        verify(cmdFactory)
+                .addEvent("some talk", LocalDateTime.of(2016, 4, 22, 10, 0), LocalDateTime.of(2016, 4, 22, 11, 0));
+    }
+
+    @Test
+    public void testAddRecurringEvents() throws Exception {
+        //Partition: Recurring Events
+        parser.parse("CS2103 Lecture from 15/1/16 4pm to 6pm every week");
+        verify(cmdFactory).addRecurringEvent("CS2103 Lecture", LocalDateTime.of(2016, 1, 15, 16, 0),
+                LocalDateTime.of(2016, 1, 15, 18, 0), Period.ofWeeks(1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidOptions() throws Exception {
+        parser.parse("imagination from today 2pm to 3pm by tomorrow");
     }
 }
