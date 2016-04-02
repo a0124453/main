e97d9c0dec0842aa2aa3d64b80fd017da8d26ba6
mhaikalr <mhaikalr1992@gmail.com>
2016-04-02 16:42:16 +0800

    New Event class.

---
+++ src/lifetracker/calendar/Event.java
@@ -0,0 +1,52 @@
+package lifetracker.calendar;
+
+import java.time.LocalDateTime;
+
+public class Event extends DeadlineTask {
+
+    private LocalDateTime startDateTime;
+
+    public Event(String name, LocalDateTime start, LocalDateTime end) {
+        super(name, end);
+        this.startDateTime = start;
+    }
+
+    public Event(Event entry) {
+        super(entry);
+        this.startDateTime = entry.startDateTime;
+    }
+
+    @Override
+    public LocalDateTime getDateTime(CalendarProperty property) {
+        if (property.equals(CalendarProperty.START)) {
+            return startDateTime;
+        }
+        return super.getDateTime(property);
+    }
+
+    @Override
+    public void setDateTime(CalendarProperty property, LocalDateTime dateTime) {
+        if (property.equals(CalendarProperty.START)) {
+            this.startDateTime = dateTime;
+        } else {
+            super.setDateTime(property, dateTime);
+        }
+    }
+
+    @Override
+    public boolean isProperty(CalendarProperty property) {
+        switch (property) {
+            case ONGOING :
+                boolean hasStarted = LocalDateTime.now().isAfter(startDateTime);
+                boolean isOver = isProperty(CalendarProperty.OVER);
+                return hasStarted && !isOver;
+            case TODAY :
+                boolean startsToday = (LocalDateTime.now().equals(startDateTime));
+                boolean isOngoing = isProperty(CalendarProperty.ONGOING);
+                return startsToday || isOngoing;
+            default :
+                return super.isProperty(property);
+        }
+    }
+
+}
