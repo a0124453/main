2e9a03ed60fbe9b76c66c789a59703426079924d
mhaikalr <mhaikalr1992@gmail.com>
2016-04-02 18:10:17 +0800

    New class RecurringTask.

--- src/lifetracker/calendar/CalendarProperty.java
+++ src/lifetracker/calendar/CalendarProperty.java
@@ -2,6 +2,6 @@ package lifetracker.calendar;
 
 public enum CalendarProperty {
 
-    START, END, OVER, RECURRING, ONGOING, ACTIVE, TODAY;
+    START, END, OVER, RECURRING, ONGOING, ACTIVE, TODAY, LIMIT;
 
 }
---
+++ src/lifetracker/calendar/RecurringTask.java
@@ -0,0 +1,128 @@
+package lifetracker.calendar;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+
+public class RecurringTask extends DeadlineTask {
+
+    private static final int INF_LIMIT = -1;
+    private static final int DATE_LIMIT = -2;
+    private static final String MESSAGE_ERROR_NEGATIVE_LIMIT = "Number of occurences must be positive!";
+    private static final String MESSAGE_ERROR_INVALID_LIMIT_DATE = "Deadline cannot be before limit date!";
+
+    Period period;
+    int currentOccurence = 1;
+    int limitOccurences;
+    LocalDate limitDate;
+
+    public RecurringTask(String name, LocalDateTime deadline, Period period) {
+        super(name, deadline);
+        this.period = period;
+        this.limitOccurences = INF_LIMIT;
+    }
+
+    public RecurringTask(String name, LocalDateTime deadline, Period period, int limit) {
+        super(name, deadline);
+        this.period = period;
+        if (limit <= 0) {
+            throw new IllegalArgumentException(MESSAGE_ERROR_NEGATIVE_LIMIT);
+        }
+        this.limitOccurences = limit;
+    }
+
+    public RecurringTask(String name, LocalDateTime deadline, Period period, LocalDate limit) {
+        super(name, deadline);
+        if (limit.isBefore(deadline.toLocalDate())) {
+            throw new IllegalArgumentException(MESSAGE_ERROR_INVALID_LIMIT_DATE);
+        }
+        this.period = period;
+        this.limitDate = limit;
+        this.limitOccurences = DATE_LIMIT;
+    }
+
+    public RecurringTask(RecurringTask entry) {
+        super(entry);
+        this.period = entry.period;
+        this.limitOccurences = entry.limitOccurences;
+        this.currentOccurence = entry.currentOccurence;
+        this.limitDate = entry.limitDate;
+    }
+
+    @Override
+    public void setDateTime(CalendarProperty property, LocalDateTime dateTime) {
+        switch (property) {
+            case LIMIT :
+                limitOccurences = DATE_LIMIT;
+                if (dateTime.toLocalDate().isBefore(this.getDateTime(CalendarProperty.END).toLocalDate())) {
+                    throw new IllegalArgumentException(MESSAGE_ERROR_INVALID_LIMIT_DATE);
+                } else {
+                    this.limitDate = dateTime.toLocalDate();
+                }
+                break;
+            case END :
+                if (limitOccurences == DATE_LIMIT && this.limitDate.isBefore(dateTime.toLocalDate())) {
+                    throw new IllegalArgumentException(MESSAGE_ERROR_INVALID_LIMIT_DATE);
+                }
+                // Fallthrough
+            default :
+                super.setDateTime(property, dateTime);
+        }
+    }
+
+    @Override
+    public void setPeriod(Period period) {
+        this.period = period;
+    }
+
+    @Override
+    public Period getPeriod() {
+        return period;
+    }
+
+    /*
+     * @Override public void mark() { // TODO Auto-generated method stub
+     * super.mark(); }
+     */
+
+    @Override
+    public boolean isProperty(CalendarProperty property) {
+        switch (property) {
+            case RECURRING :
+                return true;
+            default :
+                return super.isProperty(property);
+        }
+    }
+
+    public boolean hasNext() {
+        if (this.limitOccurences == INF_LIMIT) {
+            return true;
+        } else if (this.limitOccurences == DATE_LIMIT) {
+            return this.getDateTime(CalendarProperty.END).toLocalDate().plus(period).isBefore(limitDate);
+        } else {
+            return currentOccurence < limitOccurences;
+        }
+    }
+
+    public void updateToNext() {
+        if (hasNext()) {
+            LocalDateTime currentDeadline = getDateTime(CalendarProperty.END);
+            setDateTime(CalendarProperty.END, currentDeadline.plus(this.period));
+            currentOccurence++;
+        }
+    }
+
+    public void setOccurrenceLimit(int n) {
+        if (limitOccurences == DATE_LIMIT || limitOccurences == INF_LIMIT) {
+            currentOccurence = 1;
+            limitDate = null;
+            this.limitOccurences = n;
+        } else if (n < currentOccurence) {
+            limitOccurences = currentOccurence;
+        } else {
+            limitOccurences = n;
+        }
+    }
+
+}
