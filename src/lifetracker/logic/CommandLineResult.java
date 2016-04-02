5f688f9e6b1cad5e41049f2f4c6b7ab4b99b4c39
sshang0309 <sshang@connect.ust.hk>
2016-04-02 22:16:54 +0800

    change the implementation of the ExecutionResult

--- src/lifetracker/logic/CommandLineResult.java
+++ src/lifetracker/logic/CommandLineResult.java
@@ -1,10 +1,6 @@
 package lifetracker.logic;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 import java.time.temporal.TemporalAmount;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,16 +12,6 @@ public class CommandLineResult implements ExecuteResult {
     private List<LogicDeadline> deadlineList;
     private List<LogicFloating> floatingList;
     private CommandType commandType;
-
-    private static final FormatStyle DATE_STYLE = FormatStyle.MEDIUM;
-    private static final FormatStyle TIME_STYLE = FormatStyle.SHORT;
-
-    private static final String DAY_FIELD = "day(s)";
-    private static final String MONTH_FIELD = "month(s)";
-    private static final String YEAR_FIELD = "year(s)";
-
-    private static final String MINUTE_FIELD = "minute(s)";
-    private static final String HOUR_FIELD = "hour(s)";
 
     public CommandLineResult() {
         this.eventList = new ArrayList<>();
@@ -46,87 +32,52 @@ public class CommandLineResult implements ExecuteResult {
     }
 
     @Override
-    public List<List<String>> getEventList() {
+    public List<LogicEvent> getEventList() {
         return eventList;
     }
 
     @Override
-    public List<List<String>> getTaskList() {
-        return taskList;
+    public List<LogicDeadline> getDeadlineList() {
+        return deadlineList;
+    }
+    
+    @Override
+    public List<LogicFloating> getFloatingList() {
+        return floatingList;
     }
 
     @Override
-    public void addTaskLine(int id, String name, boolean isActive, LocalDateTime deadline, TemporalAmount period) {
-        List<String> record = new ArrayList<>();
-        record.add(String.valueOf(id));
-        record.add(name);
-        record.add(Boolean.toString(isActive));
-
-        if (deadline == null) {
-            record.add("");
-            record.add("");
-        } else {
-            record.add(deadline.format(DateTimeFormatter.ofLocalizedDateTime(DATE_STYLE, TIME_STYLE)));
-
-            if (period == null) {
-                record.add("");
-            } else {
-                record.add(convert(period));
-            }
-        }
-        
-        taskList.add(record);
+    public void addFloatingLine(int id, String name, boolean isDone) {
+        LogicFloating record = new LogicFloatingImpl();
+        record.setId(id);
+        record.setName(name);
+        record.setDone(isDone);
+        floatingList.add(record);
     }
-
+    
     @Override
-    public void addEventLine(int id, String name, boolean isActive, LocalDateTime start, LocalDateTime end,
-            TemporalAmount period) {
-        List<String> record = new ArrayList<>();
-        record.add(String.valueOf(id));
-        record.add(name);
-        record.add(Boolean.toString(isActive));
-        record.add(start.format(DateTimeFormatter.ofLocalizedDateTime(DATE_STYLE, TIME_STYLE)));
-        record.add(end.format(DateTimeFormatter.ofLocalizedDateTime(DATE_STYLE, TIME_STYLE)));
-
-        if (period == null) {
-            record.add("");
-        } else {
-            record.add(convert(period));
-        }
-
+    public void addDeadlineLine(int id, String name, LocalDateTime deadline, boolean isOverdue, boolean isDone, TemporalAmount period) {
+        LogicDeadline record = new LogicDeadlineImpl();
+        record.setId(id);
+        record.setName(name);
+        record.setDeadline(deadline);
+        record.setOverdue(isOverdue);
+        record.setDone(isDone);
+        record.setPeriod(period);
+        deadlineList.add(record);
+    }
+    
+    @Override
+    public void addEventLine(int id, String name, LocalDateTime start, LocalDateTime end, boolean isOverdue, boolean isDone, TemporalAmount period) {
+        LogicEvent record = new LogicEventImpl();
+        record.setId(id);
+        record.setName(name);
+        record.setStart(start);
+        record.setEnd(end);
+        record.setOverdue(isOverdue);
+        record.setDone(isDone);
+        record.setPeriod(period);
         eventList.add(record);
-    }
-
-    public String convert(TemporalAmount temporalAmount) {
-        if (temporalAmount != null) {
-            if (temporalAmount.equals(Period.ZERO) || temporalAmount.equals(Duration.ZERO)) {
-                return "";
-            } else if (temporalAmount instanceof Period) {
-                return convertPeriodToString(((Period) temporalAmount).normalized());
-            } else {
-                return convertDurationToString((Duration) temporalAmount);
-            }
-        }
-        return "";
-    }
-
-    private String convertPeriodToString(Period period) {
-        int years = period.getYears();
-        int months = period.getMonths();
-        int days = period.getDays();
-
-        return formatDuration(years, YEAR_FIELD) + formatDuration(months, MONTH_FIELD) + formatDuration(days, DAY_FIELD);
-    }
-
-    private String convertDurationToString(Duration duration) {
-        long hours = duration.toHours();
-        long minutes = duration.toMinutes() % 60;
-
-        return formatDuration(hours, HOUR_FIELD) + formatDuration(minutes, MINUTE_FIELD);
-    }
-
-    private String formatDuration(long duration, String label) {
-        return duration == 0 ? "" : duration + " " + label + " ";
     }
 
     @Override
