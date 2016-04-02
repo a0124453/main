8c8d32c55f4ccf84d51103cc7df51293526db458
mhaikalr <mhaikalr1992@gmail.com>
2016-04-02 16:24:41 +0800

    New class DeadlineTask.

---
+++ src/lifetracker/calendar/DeadlineTask.java
@@ -0,0 +1,52 @@
+package lifetracker.calendar;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class DeadlineTask extends GenericEntry {
+
+    private LocalDateTime deadline;
+
+    public DeadlineTask(String name, LocalDateTime deadline) {
+        super(name);
+        this.setDateTime(CalendarProperty.END, deadline);
+    }
+
+    public DeadlineTask(DeadlineTask entry) {
+        super(entry);
+        this.setDateTime(CalendarProperty.END, deadline);
+    }
+
+    @Override
+    public LocalDateTime getDateTime(CalendarProperty property) {
+        if (property.equals(CalendarProperty.END)) {
+            return this.deadline;
+        } else {
+            return super.getDateTime(property);
+        }
+    }
+
+    @Override
+    public void setDateTime(CalendarProperty property, LocalDateTime deadline) {
+        if (property.equals(CalendarProperty.END)) {
+            this.deadline = deadline;
+        } else {
+            super.setDateTime(property, deadline);
+        }
+    }
+
+    @Override
+    public boolean isProperty(CalendarProperty property) {
+        switch (property) {
+            case OVER :
+                return LocalDateTime.now().isAfter(this.deadline);
+            case ONGOING :
+                return LocalDateTime.now().isBefore(this.deadline);
+            case TODAY :
+                return LocalDate.now().equals(this.deadline.toLocalDate());
+            default :
+                return super.isProperty(property);
+        }
+    }
+
+}
