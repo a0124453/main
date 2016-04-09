2099355b36559e9a576a1f1839e1b0d609541ddc
sshang0309 <sshang@connect.ust.hk>
2016-04-10 02:30:33 +0800

    extract constant for logicTest

--- test/lifetracker/logic/LogicImplTest.java
+++ test/lifetracker/logic/LogicImplTest.java
@@ -22,7 +22,32 @@ import static org.mockito.Mockito.when;
 
 public class LogicImplTest {
 
-    private static final String ERROR_INVALID_COMMAND = "Invalid Command: null";
+    //test command
+    private static final String COMMAND_SAVE = "saveat ";
+    private static final String COMMAND_EXIT = "exit";
+    private static final String COMMAND_HELP = "help";
+    private static final String COMMAND_ADD_FIRST = "add first meeting";
+    private static final String COMMAND_ADD_SECOND = "add second meeting by 2016-12-31 23:59:59";
+    private static final String COMMAND_ADD_THIRD = "add third meeting from 2017-01-01 00:00:00 to 2017-01-01 23:59:59";
+    private static final String COMMAND_ERROR_MONTH1 = "add error meeting by 2016-00-31 23:59:59";
+    private static final String COMMAND_ERROR_MONTH2 = "add error meeting by 2016-13-31 23:59:59";
+    private static final String COMMAND_ERROR_DAY1 = "add error meeting by 2016-12-00 23:59:59";
+    private static final String COMMAND_ERROR_DAY2 = "add error meeting by 2016-12-32 23:59:59";
+    private static final String COMMAND_ERROR_HOUR1 = "add error meeting by 2016-12-31 -1:59:59";
+    private static final String COMMAND_ERROR_HOUR2 = "add error meeting by 2016-12-31 24:59:59";
+    private static final String COMMAND_ERROR_MINUTE1 = "add error meeting by 2016-12-31 23:-1:59";
+    private static final String COMMAND_ERROR_MINUTE2 = "add error meeting by 2016-12-31 23:60:59";
+    private static final String COMMAND_ERROR_SECOND1 = "add error meeting by 2016-12-31 23:59:-1";
+    private static final String COMMAND_ERROR_SECOND2 = "add error meeting by 2016-12-31 23:59:60";
+    
+    //expected comment after execution
+    private static final String COMMENT_SAVE = "Calendar is saved at ";
+    private static final String COMMENT_ADD_FIRST = "\"first meeting\" is added.";
+    private static final String COMMENT_ADD_SECOND = "\"second meeting\" is added.";
+    private static final String COMMENT_ADD_THIRD = "\"third meeting\" is added.";
+
+    //error comment after execution
+    private static final String COMMENT_INVALID_COMMAND = "Invalid Command: null";
     
     //test store constant
     private static final String DEFAULT_CONFIG_FILENAME = "config.properties";
@@ -50,22 +75,22 @@ public class LogicImplTest {
 
         logicTest = new LogicImpl(parser, storage);
 
-        saveat.setComment("Calendar is saved at " + TEST_SAVEAT_FILE);
+        saveat.setComment(COMMENT_SAVE + TEST_SAVEAT_FILE);
         saveat.setType(CommandType.SAVE);
         
         exit.setType(CommandType.EXIT);
         help.setType(CommandType.HELP);
         
-        expected1.setComment("\"first meeting\" is added.");
+        expected1.setComment(COMMENT_ADD_FIRST);
         expected1.setType(CommandType.DISPLAY);
 
-        expected2.setComment("\"second meeting\" is added.");
+        expected2.setComment(COMMENT_ADD_SECOND);
         expected2.setType(CommandType.DISPLAY);
 
-        expected3.setComment("\"third meeting\" is added.");
+        expected3.setComment(COMMENT_ADD_THIRD);
         expected3.setType(CommandType.DISPLAY);
 
-        error.setComment(ERROR_INVALID_COMMAND);
+        error.setComment(COMMENT_INVALID_COMMAND);
         error.setType(CommandType.ERROR);
     }
 
@@ -119,26 +144,26 @@ public class LogicImplTest {
 
     @Test
     public void testSaveat() {
-        assertEquals(saveat.getComment(), logicTest.executeCommand("saveat " + TEST_SAVEAT_FILE).getComment());
-        assertEquals(saveat.getEventList(), logicTest.executeCommand("saveat " + TEST_SAVEAT_FILE).getEventList());
-        assertEquals(saveat.getTaskList(), logicTest.executeCommand("saveat " + TEST_SAVEAT_FILE).getTaskList());
-        assertEquals(saveat.getType(), logicTest.executeCommand("saveat " + TEST_SAVEAT_FILE).getType());
+        assertEquals(saveat.getComment(), logicTest.executeCommand(COMMAND_SAVE + TEST_SAVEAT_FILE).getComment());
+        assertEquals(saveat.getEventList(), logicTest.executeCommand(COMMAND_SAVE + TEST_SAVEAT_FILE).getEventList());
+        assertEquals(saveat.getTaskList(), logicTest.executeCommand(COMMAND_SAVE + TEST_SAVEAT_FILE).getTaskList());
+        assertEquals(saveat.getType(), logicTest.executeCommand(COMMAND_SAVE + TEST_SAVEAT_FILE).getType());
     }
     
     @Test
     public void testExit() {
-        assertEquals(exit.getComment(), logicTest.executeCommand("exit").getComment());
-        assertEquals(exit.getEventList(), logicTest.executeCommand("exit").getEventList());
-        assertEquals(exit.getTaskList(), logicTest.executeCommand("exit").getTaskList());
-        assertEquals(exit.getType(), logicTest.executeCommand("exit").getType());
+        assertEquals(exit.getComment(), logicTest.executeCommand(COMMAND_EXIT).getComment());
+        assertEquals(exit.getEventList(), logicTest.executeCommand(COMMAND_EXIT).getEventList());
+        assertEquals(exit.getTaskList(), logicTest.executeCommand(COMMAND_EXIT).getTaskList());
+        assertEquals(exit.getType(), logicTest.executeCommand(COMMAND_EXIT).getType());
     }
     
     @Test
     public void testHelp() {
-        assertEquals(help.getComment(), logicTest.executeCommand("help").getComment());
-        assertEquals(help.getEventList(), logicTest.executeCommand("help").getEventList());
-        assertEquals(help.getTaskList(), logicTest.executeCommand("help").getTaskList());
-        assertEquals(help.getType(), logicTest.executeCommand("help").getType());
+        assertEquals(help.getComment(), logicTest.executeCommand(COMMAND_HELP).getComment());
+        assertEquals(help.getEventList(), logicTest.executeCommand(COMMAND_HELP).getEventList());
+        assertEquals(help.getTaskList(), logicTest.executeCommand(COMMAND_HELP).getTaskList());
+        assertEquals(help.getType(), logicTest.executeCommand(COMMAND_HELP).getType());
     }
 
     @Test
@@ -146,12 +171,12 @@ public class LogicImplTest {
         AddCommand object = mock(AddCommand.class);
         CalendarList list = mock(CalendarList.class);
 
-        when(object.getComment()).thenReturn("\"first meeting\" is added.");
+        when(object.getComment()).thenReturn(COMMENT_ADD_FIRST);
         when(object.execute(any(CalendarList.class))).thenReturn(list);
-        when(parser.parse("add first meeting")).thenReturn(object);
+        when(parser.parse(COMMAND_ADD_FIRST)).thenReturn(object);
 
-        ExecuteResult actual = logicTest.executeCommand("add first meeting");
-        verify(parser).parse("add first meeting");
+        ExecuteResult actual = logicTest.executeCommand(COMMAND_ADD_FIRST);
+        verify(parser).parse(COMMAND_ADD_FIRST);
 
         assertEquals(expected1.getComment(), actual.getComment());
         assertEquals(expected1.getEventList(), actual.getEventList());
@@ -165,12 +190,12 @@ public class LogicImplTest {
         AddCommand object = mock(AddCommand.class);
         CalendarList list = mock(CalendarList.class);
 
-        when(object.getComment()).thenReturn("\"second meeting\" is added.");
+        when(object.getComment()).thenReturn(COMMENT_ADD_SECOND);
         when(object.execute(any(CalendarList.class))).thenReturn(list);
-        when(parser.parse("add second meeting by 2016-12-31 23:59:59")).thenReturn(object);
+        when(parser.parse(COMMAND_ADD_SECOND)).thenReturn(object);
 
-        ExecuteResult actual = logicTest.executeCommand("add second meeting by 2016-12-31 23:59:59");
-        verify(parser).parse("add second meeting by 2016-12-31 23:59:59");
+        ExecuteResult actual = logicTest.executeCommand(COMMAND_ADD_SECOND);
+        verify(parser).parse(COMMAND_ADD_SECOND);
 
         assertEquals(expected2.getComment(), actual.getComment());
         assertEquals(expected2.getEventList(), actual.getEventList());
@@ -184,13 +209,13 @@ public class LogicImplTest {
         AddCommand object = mock(AddCommand.class);
         CalendarList list = mock(CalendarList.class);
 
-        when(object.getComment()).thenReturn("\"third meeting\" is added.");
+        when(object.getComment()).thenReturn(COMMENT_ADD_THIRD);
         when(object.execute(any(CalendarList.class))).thenReturn(list);
-        when(parser.parse("add third meeting from 2017-01-01 00:00:00 to 2017-01-01 23:59:59")).thenReturn(object);
+        when(parser.parse(COMMAND_ADD_THIRD)).thenReturn(object);
 
         ExecuteResult actual = logicTest
-                .executeCommand("add third meeting from 2017-01-01 00:00:00 to 2017-01-01 23:59:59");
-        verify(parser).parse("add third meeting from 2017-01-01 00:00:00 to 2017-01-01 23:59:59");
+                .executeCommand(COMMAND_ADD_THIRD);
+        verify(parser).parse(COMMAND_ADD_THIRD);
 
         assertEquals(expected3.getComment(), actual.getComment());
         assertEquals(expected3.getEventList(), actual.getEventList());
@@ -202,16 +227,16 @@ public class LogicImplTest {
     //This is the boundary case for the invalid partition
     @Test
     public void testAddErrorMonth() {
-        when(parser.parse("add error meeting by 2016-00-31 23:59:59")).thenThrow(new IllegalArgumentException());
-        when(parser.parse("add error meeting by 2016-13-31 23:59:59")).thenThrow(new IllegalArgumentException());
+        when(parser.parse(COMMAND_ERROR_MONTH1)).thenThrow(new IllegalArgumentException());
+        when(parser.parse(COMMAND_ERROR_MONTH2)).thenThrow(new IllegalArgumentException());
         
-        ExecuteResult actual1 = logicTest.executeCommand("add error meeting by 2016-00-31 23:59:59");
-        verify(parser).parse("add error meeting by 2016-00-31 23:59:59");
+        ExecuteResult actual1 = logicTest.executeCommand(COMMAND_ERROR_MONTH1);
+        verify(parser).parse(COMMAND_ERROR_MONTH1);
         assertEquals(error.getComment(), actual1.getComment());
         assertEquals(error.getType(), actual1.getType());
 
-        ExecuteResult actual2 = logicTest.executeCommand("add error meeting by 2016-13-31 23:59:59");
-        verify(parser).parse("add error meeting by 2016-13-31 23:59:59");
+        ExecuteResult actual2 = logicTest.executeCommand(COMMAND_ERROR_MONTH2);
+        verify(parser).parse(COMMAND_ERROR_MONTH2);
         assertEquals(error.getComment(), actual2.getComment());
         assertEquals(error.getType(), actual2.getType());
     }
@@ -220,16 +245,16 @@ public class LogicImplTest {
     //This is the boundary case for the invalid partition
     @Test
     public void testAddErrorDay() {
-        when(parser.parse("add error meeting by 2016-12-00 23:59:59")).thenThrow(new IllegalArgumentException());
-        when(parser.parse("add error meeting by 2016-12-32 23:59:59")).thenThrow(new IllegalArgumentException());
+        when(parser.parse(COMMAND_ERROR_DAY1)).thenThrow(new IllegalArgumentException());
+        when(parser.parse(COMMAND_ERROR_DAY2)).thenThrow(new IllegalArgumentException());
         
-        ExecuteResult actual3 = logicTest.executeCommand("add error meeting by 2016-12-00 23:59:59");
-        verify(parser).parse("add error meeting by 2016-12-00 23:59:59");
+        ExecuteResult actual3 = logicTest.executeCommand(COMMAND_ERROR_DAY1);
+        verify(parser).parse(COMMAND_ERROR_DAY1);
         assertEquals(error.getComment(), actual3.getComment());
         assertEquals(error.getType(), actual3.getType());
 
-        ExecuteResult actual4 = logicTest.executeCommand("add error meeting by 2016-12-32 23:59:59");
-        verify(parser).parse("add error meeting by 2016-12-32 23:59:59");
+        ExecuteResult actual4 = logicTest.executeCommand(COMMAND_ERROR_DAY2);
+        verify(parser).parse(COMMAND_ERROR_DAY2);
         assertEquals(error.getComment(), actual4.getComment());
         assertEquals(error.getType(), actual4.getType());
     }
@@ -238,16 +263,16 @@ public class LogicImplTest {
     //This is the boundary case for the invalid partition
     @Test
     public void testAddErrorHour() {
-        when(parser.parse("add error meeting by 2016-12-31 -1:59:59")).thenThrow(new IllegalArgumentException());
-        when(parser.parse("add error meeting by 2016-12-31 24:59:59")).thenThrow(new IllegalArgumentException());
+        when(parser.parse(COMMAND_ERROR_HOUR1)).thenThrow(new IllegalArgumentException());
+        when(parser.parse(COMMAND_ERROR_HOUR2)).thenThrow(new IllegalArgumentException());
 
-        ExecuteResult actual5 = logicTest.executeCommand("add error meeting by 2016-12-31 -1:59:59");
-        verify(parser).parse("add error meeting by 2016-12-31 -1:59:59");
+        ExecuteResult actual5 = logicTest.executeCommand(COMMAND_ERROR_HOUR1);
+        verify(parser).parse(COMMAND_ERROR_HOUR1);
         assertEquals(error.getComment(), actual5.getComment());
         assertEquals(error.getType(), actual5.getType());
 
-        ExecuteResult actual6 = logicTest.executeCommand("add error meeting by 2016-12-31 24:59:59");
-        verify(parser).parse("add error meeting by 2016-12-31 24:59:59");
+        ExecuteResult actual6 = logicTest.executeCommand(COMMAND_ERROR_HOUR2);
+        verify(parser).parse(COMMAND_ERROR_HOUR2);
         assertEquals(error.getComment(), actual6.getComment());
         assertEquals(error.getType(), actual6.getType());
     }
@@ -256,16 +281,16 @@ public class LogicImplTest {
     //This is the boundary case for the invalid partition
     @Test
     public void testAddErrorMinute() {
-        when(parser.parse("add error meeting by 2016-12-31 23:-1:59")).thenThrow(new IllegalArgumentException());
-        when(parser.parse("add error meeting by 2016-12-31 23:60:59")).thenThrow(new IllegalArgumentException());
+        when(parser.parse(COMMAND_ERROR_MINUTE1)).thenThrow(new IllegalArgumentException());
+        when(parser.parse(COMMAND_ERROR_MINUTE2)).thenThrow(new IllegalArgumentException());
 
-        ExecuteResult actual7 = logicTest.executeCommand("add error meeting by 2016-12-31 23:-1:59");
-        verify(parser).parse("add error meeting by 2016-12-31 23:-1:59");
+        ExecuteResult actual7 = logicTest.executeCommand(COMMAND_ERROR_MINUTE1);
+        verify(parser).parse(COMMAND_ERROR_MINUTE1);
         assertEquals(error.getComment(), actual7.getComment());
         assertEquals(error.getType(), actual7.getType());
 
-        ExecuteResult actual8 = logicTest.executeCommand("add error meeting by 2016-12-31 23:60:59");
-        verify(parser).parse("add error meeting by 2016-12-31 23:60:59");
+        ExecuteResult actual8 = logicTest.executeCommand(COMMAND_ERROR_MINUTE2);
+        verify(parser).parse(COMMAND_ERROR_MINUTE2);
         assertEquals(error.getComment(), actual8.getComment());
         assertEquals(error.getType(), actual8.getType());
     }
@@ -274,16 +299,16 @@ public class LogicImplTest {
     //This is the boundary case for the invalid partition
     @Test
     public void testAddErrorSecond() {
-        when(parser.parse("add error meeting by 2016-12-31 23:59:-1")).thenThrow(new IllegalArgumentException());
-        when(parser.parse("add error meeting by 2016-12-31 23:59:60")).thenThrow(new IllegalArgumentException());
+        when(parser.parse(COMMAND_ERROR_SECOND1)).thenThrow(new IllegalArgumentException());
+        when(parser.parse(COMMAND_ERROR_SECOND2)).thenThrow(new IllegalArgumentException());
         
-        ExecuteResult actual9 = logicTest.executeCommand("add error meeting by 2016-12-31 23:59:-1");
-        verify(parser).parse("add error meeting by 2016-12-31 23:59:-1");
+        ExecuteResult actual9 = logicTest.executeCommand(COMMAND_ERROR_SECOND1);
+        verify(parser).parse(COMMAND_ERROR_SECOND1);
         assertEquals(error.getComment(), actual9.getComment());
         assertEquals(error.getType(), actual9.getType());
 
-        ExecuteResult actual10 = logicTest.executeCommand("add error meeting by 2016-12-31 23:59:60");
-        verify(parser).parse("add error meeting by 2016-12-31 23:59:60");
+        ExecuteResult actual10 = logicTest.executeCommand(COMMAND_ERROR_SECOND2);
+        verify(parser).parse(COMMAND_ERROR_SECOND2);
         assertEquals(error.getComment(), actual10.getComment());
         assertEquals(error.getType(), actual10.getType());
     }
