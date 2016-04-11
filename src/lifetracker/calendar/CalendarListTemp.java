b2d20d6b965fa8a8314f47db45a6dbc8dae07a4f
mhaikalr <mhaikalr1992@gmail.com>
2016-04-11 16:04:04 +0800

    Override methods to disable them in CalendarListTemp.java.

--- src/lifetracker/calendar/CalendarListTemp.java
+++ src/lifetracker/calendar/CalendarListTemp.java
@@ -1,5 +1,6 @@
 package lifetracker.calendar;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.util.TreeMap;
@@ -38,12 +39,37 @@ public class CalendarListTemp extends CalendarListImpl {
     }
 
     @Override
+    public int add(String name, LocalDateTime deadline, Period period, int limit) {
+        return 0;
+    }
+
+    @Override
+    public int add(String name, LocalDateTime deadline, Period period, LocalDate limitDate) {
+        return 0;
+    }
+
+    @Override
     public int add(String name, LocalDateTime start, LocalDateTime end) {
         return 0;
     }
 
     @Override
     public int add(String name, LocalDateTime start, LocalDateTime end, Period period) {
+        return 0;
+    }
+
+    @Override
+    public int add(String name, LocalDateTime start, LocalDateTime end, Period period, int limit) {
+        return 0;
+    }
+
+    @Override
+    public int add(String name, LocalDateTime start, LocalDateTime end, Period period, LocalDate limitDate) {
+        return 0;
+    }
+
+    @Override
+    public int add(CalendarEntry entry) {
         return 0;
     }
 
