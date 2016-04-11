ff03aec47bc44a6d08c16b28bd1db0ed14e2159f
mhaikalr <mhaikalr1992@gmail.com>
2016-04-11 20:34:28 +0800

    Complete CalendarEntry.java documentation.

--- src/lifetracker/calendar/CalendarEntry.java
+++ src/lifetracker/calendar/CalendarEntry.java
@@ -16,28 +16,39 @@ public interface CalendarEntry extends VisitableEntry {
     }
 
     /**
+     * Returns the unique ID number of the entry.
+     * 
      * @return The unique ID number of the entry.
      */
     int getId();
 
     /**
+     * Sets the ID number of the entry to {@code id}.
+     * 
      * @param id
      *            The desired ID number to assign to the entry.
      */
     void setId(int id);
 
     /**
-     * @return The description of the entry.
+     * Returns the name of the entry.
+     * 
+     * @return The name of the entry.
      */
     String getName();
 
     /**
+     * Sets the name of the entry to {@code name}.
+     * 
      * @param name
      *            The desired name to assign to the entry.
      */
     void setName(String name);
 
     /**
+     * Returns the start/end date and time of the entry depending on the
+     * {@code property} provided.
+     * 
      * @param property
      *            An {@code enum} representing the desired property.
      * 
@@ -46,6 +57,9 @@ public interface CalendarEntry extends VisitableEntry {
     LocalDateTime getDateTime(CalendarProperty property);
 
     /**
+     * Sets the start/end date and time of the entry depending on the
+     * {@code property} provided.
+     * 
      * @param property
      *            An {@code enum} representing the property to be modified.
      * 
@@ -55,12 +69,16 @@ public interface CalendarEntry extends VisitableEntry {
     void setDateTime(CalendarProperty property, LocalDateTime dateTime);
 
     /**
+     * Sets the period of the (recurring) entry to {@code period}.
+     * 
      * @param period
      *            The period to assign to the (recurring) entry.
      */
     void setPeriod(Period period);
 
     /**
+     * Returns the period of the (recurring) entry.
+     * 
      * @return The period of the (recurring) entry.
      */
     Period getPeriod();
@@ -71,6 +89,9 @@ public interface CalendarEntry extends VisitableEntry {
     void toggleActive();
 
     /**
+     * Returns a {@code boolean} to indicate whether the entry satisfies the
+     * specified property.
+     * 
      * @param property
      *            An {@code enum} representing the desired property.
      * 
@@ -80,6 +101,8 @@ public interface CalendarEntry extends VisitableEntry {
     boolean isProperty(CalendarProperty property);
 
     /**
+     * Returns an integer property of the entry.
+     * 
      * @param property
      *            An {@code enum} representing the desired property.
      * 
