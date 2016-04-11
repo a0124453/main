ce25172917f2d1f71a43f1ba157589cf1a0e54bd
Shen Yichen <2007.yichen@gmail.com>
2016-04-11 20:40:32 +0800

    Add Javadoc for parser
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/lifetracker/parser/Parser.java
+++ src/lifetracker/parser/Parser.java
@@ -2,6 +2,18 @@ package lifetracker.parser;
 
 import lifetracker.command.CommandObject;
 
+//@@author A0091173J
+
+/**
+ * Parser takes in a user input string which represents a command affecting the calendar, and returns a CommandObject
+ * that can be executed to change the calendar.
+ */
 public interface Parser {
+    /**
+     * Parses the user input into a corresponding CommandObject.
+     *
+     * @param userInput The user entered command.
+     * @return The CommandObject
+     */
     CommandObject parse(String userInput);
 }
